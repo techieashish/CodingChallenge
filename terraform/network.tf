4781d481681e4a53d8b42ece749d201288ef612b
techieashish <ashishsrivastava_18@yahoo.in>
2020-12-16 04:26:28 +0530

    fixed syntax on network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -84,3 +84,4 @@ resource "aws_route" "public-internet-igw-route" {
   route_table_id         = aws_route_table.public-route-table.id
   gateway_id             = aws_internet_gateway.production-igw.id
   destination_cidr_block = "0.0.0.0/0"
+}
